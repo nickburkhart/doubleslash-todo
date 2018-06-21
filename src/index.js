bc10db9a5e51a1f1820d0c1f7ebebc4a0871cc77
Nick Burkhart <nickburkhart@gmail.com>
2018-06-20 21:00:52 -0400

    chore: add logger for development

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -8,7 +8,21 @@ import { App } from './App';
 import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer);
+let middleware;
+if (process.env.NODE_ENV !== 'production') {
+    const logger = store => next => action => {
+        console.log('dispatching', action)
+        let result = next(action)
+        console.log('next state', store.getState())
+        return result
+    };
+    middleware = applyMiddleware(logger);
+} else {
+    middleware = applyMiddleware();
+}
+
+
+const store = createStore(rootReducer, middleware);
 
 ReactDOM.render((
     <Provider store={store}>
